156fe3d63fb21926d1757b4cbde079602947da50
Daniel Guest <ddgos@proton.me>
2025-01-28 20:54:47 +0000

    updated to allow custom move formats

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,46 @@ use clap_stdin::FileOrStdin;
 use goban::rules::game::Game;
 
 #[derive(Parser)]
+#[command(version)]
 struct Args {
+    /// whitespace separated list of x labels to use
+    x_labels: String,
+    /// whitespace separated list of y labels to use
+    y_labels: String,
     sgf: FileOrStdin
 }
 
-fn coord_to_str(x: u8, y: u8) -> String {
-    format!("{}{}", (b'a' + x) as char, y + 1)
-}
-
 fn main() -> Result<()> {
-    let Args { sgf } = Args::parse();
+    let Args { sgf, x_labels, y_labels } = Args::parse();
 
-    let game = Game::from_sgf(&sgf.contents()?).map_err(|e| anyhow!(e))?; 
+    let x_labels: Vec<&str> = x_labels.split_whitespace().collect();
+    let y_labels: Vec<&str> = y_labels.split_whitespace().collect();
+
+    let game = Game::from_sgf(&sgf.contents()?).map_err(|e| anyhow!(e))?;
 
     let (width, height) = game.size();
-    if width > 19 {
-        return Err(anyhow!("Maximum allowed board size 19. width: {}", width));
+    if width as usize > x_labels.len() {
+        return Err(anyhow!(
+            "Not enough x labels supplied. width: {}, x labels: {:?}",
+            width,
+            x_labels
+        ));
     }
-    if height > 19 {
-        return Err(anyhow!("Maximum allowed board size 19. height: {}", height));   
+    if height as usize > y_labels.len() {
+        return Err(anyhow!(
+            "Not enough y labels supplied. height: {}, y labels: {:?}",
+            height,
+            y_labels
+        ));
     }
 
-    let legal_moves: Vec<_> = game.legals().map(|(x, y)| coord_to_str(x, y)).collect();
+    let legal_moves: Vec<_> = game.legals().map(|(x, y)| {
+        format!(
+            "{}{}",
+             x_labels.get(x as usize).expect("should have checked there were enough labels"),
+             y_labels.get(y as usize).expect("should have checked there were enough labels"),
+        )
+    }).collect();
     print!("({})", legal_moves.join("|"));
 
     Ok(())
